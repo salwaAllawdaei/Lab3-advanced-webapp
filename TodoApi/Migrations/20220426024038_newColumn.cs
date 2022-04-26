142e3c17cac079e4294310e3f27ea033b673611d
salwa <1496118@cegep-heritage.qc.ca>
2022-04-25 23:24:39 -0400

    column added

---
+++ TodoApi/Migrations/20220426024038_newColumn.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoApi.Migrations
+{
+    public partial class newColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AMustDo",
+                table: "TodoItems");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Deadline",
+                table: "TodoItems",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Reward",
+                table: "TodoItems",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Reward",
+                table: "TodoItems");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Deadline",
+                table: "TodoItems",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AMustDo",
+                table: "TodoItems",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- TodoApi/Model/TodoItem.cs
+++ TodoApi/Model/TodoItem.cs
@@ -7,7 +7,7 @@ public class TodoItem
     public uint TodoItemId { get; set; }
     public string? Task { get; set; }
     public string? Instructions { get; set; }
-    public DateTime Deadline { get; set; } = DateTime.Now.AddDays(7);
-    public bool AMustDo { get; set; } = true;
+    public decimal Reward { get; set; }
+    public DateTime? Deadline { get; set; }
     public bool IsComplete { get; set; } = false;
 }
